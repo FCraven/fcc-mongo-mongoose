654de2ee1a6ee5c64eb422d3952b46150a2d23c7
FCraven <craven.francis@gmail.com>
2021-05-26 13:59:04 -0400

    trying to squash bugs

--- myApp.js
+++ myApp.js
@@ -18,7 +18,7 @@ const arrayOfPeople = [
     age: 1,
     favoriteFoods: ['everything']
   }
-]
+];
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
