228ce1f2ad562a7ad00b337a5fc701693727592d
FCraven <craven.francis@gmail.com>
2021-05-26 12:30:36 -0400

    fixing import error for Schema

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const mongoose, { Schema }= require('mongoose');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
