b6c32e092495bcb9ce9cf372c365f867aa63eeb6
FCraven <craven.francis@gmail.com>
2021-05-26 14:02:21 -0400

    stupid

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ router.post("/create-many-people", function (req, res, next) {
           return next(err);
         }
         res.json(pers);
-        // Person.remove().exec();
+        Person.remove().exec();
       });
     });
   });
